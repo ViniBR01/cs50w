1a7a89e12f28ba5900dd8c68f5c638256fa285b1
ViniBR01 <vinicius@rice.edu>
2021-01-08 17:32:58 -0600

    Refactor search form to use Django forms

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from django import forms
 from . import util
 from markdown2 import markdown
 
+class SearchForm(forms.Form):
+    search = forms.CharField(label="Search")
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": SearchForm(),
     })
 
 def entry(request, title):
@@ -14,9 +18,11 @@ def entry(request, title):
         html_entry = markdown(entry)
         return render(request, "encyclopedia/entry.html", {
             "title": title,
-            "entry": html_entry
+            "entry": html_entry,
+            "form": SearchForm(),
         })
     else:
         return render(request, "encyclopedia/404.html", {
-            "title": title
+            "title": title,
+            "form": SearchForm(),
         })
