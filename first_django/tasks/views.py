961ace41fee3a37a1bf5393986b7ecb1a708ccc3
ViniBR01 <vinicius@rice.edu>
2021-01-08 13:15:23 -0600

    Refactor form to use Django forms

--- first_django/tasks/views.py
+++ first_django/tasks/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django import forms
 
 tasks = ["task1", "task2", "task3"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
@@ -9,4 +13,6 @@ def index(request):
     })
 
 def add(request):
-    return render (request, "tasks/add.html")
+    return render (request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
