f6e4e1c386ff2d0967fbd99873691b674ede0662
ViniBR01 <vinicius@rice.edu>
2021-01-08 10:20:36 -0600

    Cretae new view for tasks app

--- first_django/tasks/views.py
+++ first_django/tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+tasks = ["task1", "task2", "task3"]
+
 # Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
