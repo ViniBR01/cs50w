5f822b7948ef0874303cc91465a2116527052de9
ViniBR01 <vinicius@rice.edu>
2021-01-15 21:53:21 -0600

    Create options for the category model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,7 +6,33 @@ class User(AbstractUser):
     pass
 
 class Category(models.Model):
-    pass
+    NOT_DEFINED = 'ND'
+    CLOTHING = 'CL'
+    BOOKS = 'BK'
+    ELECTRONICS = 'EL'
+    HOME = 'HM'
+    FOOD = 'FD'
+    BEAUTY = 'BE'
+    TOYS = 'TY'
+    SPORTS = 'SP'
+    AUTOMOTIVE = 'AT'
+    CATEGORIES_CHOICES = [
+        (NOT_DEFINED, 'Not defined'),
+        (CLOTHING, 'Clothing, Shoes, Jewelry and Watches'),
+        (BOOKS, 'Books and School Suplies'),
+        (ELECTRONICS, 'Electronics and Computers'),
+        (HOME, 'Home, Garden and Tools'),
+        (FOOD, 'Food and Groceries'),
+        (BEAUTY, 'Beauty and Health'),
+        (TOYS, 'Toys, Kids and Baby'),
+        (SPORTS, 'Sports and Outdoors'),
+        (AUTOMOTIVE, 'Automotive and Industrial'),
+    ]
+    title = models.CharField(
+        max_length=2,
+        choices=CATEGORIES_CHOICES,
+        default=NOT_DEFINED,
+    )
 
 class Listing(models.Model):
     pass
