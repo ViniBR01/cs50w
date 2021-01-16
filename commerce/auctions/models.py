6f83585082daf94cbc9e22d172661bf1f508533b
ViniBR01 <vinicius@rice.edu>
2021-01-15 20:35:04 -0600

    Create base models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    pass
+
+class Listing(models.Model):
+    pass
+
+class Bid(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
