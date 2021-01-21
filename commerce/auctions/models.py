7073b0a3ac5fbd4ae2586633f14a68642eed08bb
ViniBR01 <vinicius@rice.edu>
2021-01-21 09:34:14 -0600

    Remove category model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,50 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class Category(models.Model):
-    NOT_DEFINED = 'ND'
-    CLOTHING = 'CL'
-    BOOKS = 'BK'
-    ELECTRONICS = 'EL'
-    HOME = 'HM'
-    FOOD = 'FD'
-    BEAUTY = 'BE'
-    TOYS = 'TY'
-    SPORTS = 'SP'
-    AUTOMOTIVE = 'AT'
-    CATEGORY_CHOICES = [
-        (NOT_DEFINED, 'Not defined'),
-        (CLOTHING, 'Clothing, Shoes, Jewelry and Watches'),
-        (BOOKS, 'Books and School Suplies'),
-        (ELECTRONICS, 'Electronics and Computers'),
-        (HOME, 'Home, Garden and Tools'),
-        (FOOD, 'Food and Groceries'),
-        (BEAUTY, 'Beauty and Health'),
-        (TOYS, 'Toys, Kids and Baby'),
-        (SPORTS, 'Sports and Outdoors'),
-        (AUTOMOTIVE, 'Automotive and Industrial'),
-    ]
-    title = models.CharField(
-        max_length=2,
-        choices=CATEGORY_CHOICES,
-        default=NOT_DEFINED,
-    )
+class User(AbstractUser):
+    pass
 
 class Listing(models.Model):
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="listings"
+    )
     title = models.CharField(max_length=64)
     description = models.TextField()
     price = models.DecimalField(max_digits=12, decimal_places=2)
     date = models.DateTimeField(auto_now=True)
     closed = models.BooleanField(default=False)
     image = models.URLField() #optional
-    category = models.ForeignKey(
-        Category, 
-        on_delete=models.CASCADE, 
-        related_name="listings",
-    ) #optional
-
-class User(AbstractUser):
-    watchlist = models.ManyToManyField(Listing, blank=True, related_name="watchers")
+    category = models.CharField(max_length=2) #optional
 
 class Bid(models.Model):
     author = models.ForeignKey(
@@ -63,3 +35,5 @@ class Comment(models.Model):
     )
     text = models.TextField(max_length=1024)
     date = models.DateTimeField(auto_now=True)
+
+#todo: a class for watchlists
