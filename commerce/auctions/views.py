739819a6fd95697b2aaa4f2f15bc16305c8f8a22
ViniBR01 <vinicius@rice.edu>
2021-01-21 09:34:54 -0600

    Add user info to the create request; enforce authentication to create new listing

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django import forms
 
-from .models import User, Category, Listing, Bid, Comment
+from .models import User, Listing, Bid, Comment
 
 # Djago forms
 class CreateListingForm(forms.Form):
@@ -104,12 +105,25 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
+@login_required(login_url='login')
 def create(request):
     if request.method == "POST":
-        #Receive form info
+        # Receive form info
         form = CreateListingForm(request.POST)
         if form.is_valid():
-            # Use data from form.cleaned_data: e.g., title = form.cleaned_data["title"]
+            # Use data from form.cleaned_data
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data["description"]
+            starting_bid = form.cleaned_data["starting_bid"]
+            image_url = form.cleaned_data["image_url"]
+            listing = Listing(
+                author=request.user,
+                title=title, 
+                description=description,
+                price=starting_bid,
+                image=image_url,
+            )
+            listing.save()
             return HttpResponseRedirect(reverse("index"))
         else:
             # Error message for user about form error:
@@ -117,4 +131,8 @@ def create(request):
         pass
     else:
         form = CreateListingForm()
-        return render(request, "auctions/create.html", {'form': form})
+        return render(request, "auctions/create.html",
+            {
+                'form': form
+            }
+        )
