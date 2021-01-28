195a1f64dd5c5362212132a939920f6634b19ab5
ViniBR01 <vinicius@rice.edu>
2021-01-27 22:42:42 -0600

    Add listing to the bidding model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -28,6 +28,12 @@ class Bid(models.Model):
         on_delete=models.CASCADE, 
         related_name="bids"
     )
+    listing = models.ForeignKey(
+        Listing,
+        on_delete=models.CASCADE, 
+        related_name="comments",
+        default="",
+    )
     value = models.DecimalField(max_digits=12, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
 
