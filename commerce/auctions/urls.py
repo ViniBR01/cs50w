9270d31505a8aac9471423e25bdcfacb3e185bc7
ViniBR01 <vinicius@rice.edu>
2021-01-27 20:16:49 -0600

    Create url for closing listing

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("create", views.create, name="create"),
     path("item/<int:item_id>", views.item, name="item"),
     path("item/<int:item_id>/watch", views.watch, name="watch"),
+    path("item/<int:item_id>/close", views.close, name="close"),
     path("item/<int:item_id>/comment", views.comment, name="comment"),
     path("categories", views.categories, name="categories"),
     path("categories/<slug:cat_id>", views.categorized, name="categorized"),
